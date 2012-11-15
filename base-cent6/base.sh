fcbcca9dc92ebe402b7ed20a8131ffde856f8f3f
Hiroaki Nakamura <hnakamur@gmail.com>
2012-11-15 13:45:08 +0900

    Install epel using rpm at iij.ad.jp instead of creating repo file to avoid network failures

--- base-cent6/base.sh
+++ base-cent6/base.sh
@@ -2,13 +2,7 @@
 
 sed -i "s/^.*requiretty/#Defaults requiretty/" /etc/sudoers
 
-cat > /etc/yum.repos.d/epel.repo << EOM
-[epel]
-name=epel
-baseurl=http://download.fedoraproject.org/pub/epel/6/\$basearch
-enabled=1
-gpgcheck=0
-EOM
+rpm -i http://ftp.iij.ad.jp/pub/linux/fedora/epel/6/x86_64/epel-release-6-7.noarch.rpm
 
 yum -y install gcc make gcc-c++ kernel-devel-`uname -r` zlib-devel openssl-devel readline-devel sqlite-devel perl wget
 
